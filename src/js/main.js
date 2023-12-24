0fd1705ceacd151384705c1e275ddeb6396a004f
savitskicosta <Const.Savitski@gmail.com>
2023-12-24 22:17:15 +0300

    add new features in js - link

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
 const close = document.querySelector('.menu__close');
 const overlay = document.querySelector('.menu__overlay');
+const link = document.querySelector('.menu__link');
 
 function toggleMenu() {
 	menu.classList.toggle('active');
@@ -10,3 +11,4 @@ function toggleMenu() {
 hamburger.addEventListener('click', toggleMenu);
 close.addEventListener('click', toggleMenu);
 overlay.addEventListener('click', toggleMenu);
+link.addEventListener('click', toggleMenu);

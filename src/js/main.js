a57ce0d2d451d8d770920b2e85b741867b871151
savitskicosta <Const.Savitski@gmail.com>
2023-12-24 22:38:11 +0300

    add new features in js - pageup

--- src/js/main.js
+++ src/js/main.js
@@ -3,12 +3,25 @@ const menu = document.querySelector('.menu');
 const close = document.querySelector('.menu__close');
 const overlay = document.querySelector('.menu__overlay');
 const link = document.querySelector('.menu__link');
+const pageUpElement = document.querySelector('.pageup');
 
+//-- function
+// menu
 function toggleMenu() {
 	menu.classList.toggle('active');
 }
+// UP page
+function pageUp() {
+	if (window.scrollY > 300) {
+		pageUpElement.style.display = 'block';
+	} else {
+		pageUpElement.style.display = 'none';
+	}
+}
 
+//-- launch
 hamburger.addEventListener('click', toggleMenu);
 close.addEventListener('click', toggleMenu);
 overlay.addEventListener('click', toggleMenu);
 link.addEventListener('click', toggleMenu);
+window.onscroll = pageUp;
